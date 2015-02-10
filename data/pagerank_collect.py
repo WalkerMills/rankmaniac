8586305e1e48f0b0402f0c35a6f637ff268ef34e
Walker Mills <walker.mills7@gmail.com>
2015-02-09 21:21:40 -0800

    Tracks collector (for local testing)
    I wrote a simple collector to emulate the behavior of the Amazon EMR
    collector that writes mapper output to temporary files, grouped by key,
    and uses them to feed the reducer.  It was not necessary to use with the
    process mapper/reducer, so it has been slightly hardcoded for the
    pagerank step.

---
+++ data/pagerank_collect.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python2
+
+from __future__ import with_statement
+
+import os
+import subprocess
+import sys
+
+DATA_DIR = "tmp"
+PREFIX = DATA_DIR + "/part-"
+REDUCER = "./pagerank_reduce.py"
+
+def main(argv):
+    for line in sys.stdin:
+        line = line.rstrip()
+        key, _, value = line.partition("\t")
+        _, _, node = key.partition(":")
+
+        with open(PREFIX + node, "a") as f:
+            f.write(line + "\n")
+
+    for filename in os.listdir(DATA_DIR):
+        local_path = "{}/{}".format(DATA_DIR, filename)
+        with open(local_path, "r") as f:
+            proc = subprocess.Popen(REDUCER, stdin=subprocess.PIPE)
+            proc.communicate(''.join(f.readlines()))
+        os.remove(local_path)
+
+if __name__ == "__main__":
+    main(sys.argv)
