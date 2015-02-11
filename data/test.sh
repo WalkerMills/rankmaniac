5a60126718aedba1ec5cf6484ce39cd5aced3600
Walker Mills <walker.mills7@gmail.com>
2015-02-11 01:08:30 -0800

    Adds local test script
    I wrote a simple bash script to automate the local testing.  It takes a
    single command line argument, the path of the initial data.

---
+++ data/test.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+DATA="$1"
+INPUT="./input.txt"
+OUTPUT="./output.txt"
+
+python2 pagerank_map.py < $DATA | sort | python2 pagerank_collect.py | python2 process_map.py | sort | python2 process_reduce.py > $OUTPUT && cp $OUTPUT $INPUT
+
+COUNT=1
+
+while  [[ `grep -c 'FinalRank' $OUTPUT` -eq 0 ]]; do
+    python2 pagerank_map.py < $INPUT | sort | python2 pagerank_collect.py | python2 process_map.py | sort | python2 process_reduce.py > $OUTPUT && cp $OUTPUT $INPUT    
+    COUNT=$((COUNT + 1))
+done
+
+rm $INPUT
+echo "Converged in $COUNT iterations"
+sed 's/FinalRank://' $OUTPUT | sort -r | head -n 25
